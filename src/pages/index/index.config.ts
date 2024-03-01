7d390360eff1d0d75ebdf4f3b8593292dc18d694
Mystuddent <1005605317@qq.com>
2024-03-01 18:03:07 +0800

    初始化

---
+++ src/pages/index/index.config.ts
@@ -0,0 +1,7 @@
+export default definePageConfig({
+  navigationBarTitleText: "首页",
+  usingComponents: {
+    "w-record": "./components/record/index",
+    "w-audio": "./components/w-audio/index",
+  },
+});
---
+++ src/pages/index/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <view class="index">
+    <w-record @onEnd="handleRecordEnd" />
+  </view>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import "./index.scss";
+</script>
